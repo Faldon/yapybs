9c1ea2f583f0e0aa84b0db347135a8aed5df1d0e
Thomas Pulzer <t.pulzer@thesecretgamer.de>
2015-04-15 14:51:16 +0200

    Make app production-ready.
    Provided example settings for production environment.
    Changed wsgi to load a production settings file.

--- yapybs/wsgi.py
+++ yapybs/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/home/thopu/PycharmProjects/yapybs')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yapybs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yapybs.production")
 
 application = get_wsgi_application()
