d18bb243edcc86936afd3fcfccf00fe2048f242f
Thomas Pulzer <t.pulzer@thesecretgamer.de>
2015-04-16 20:57:04 +0200

    Fixed a typo in the index view template and watched for PEP8 compliance.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -4,17 +4,17 @@ from django.conf import settings
 
 def current_date(request):
     return {
-        'current_date' : datetime.date.today()
+        'current_date': datetime.date.today()
     }
 
 
 def brand_text(request):
     return {
-        'brand_text' : getattr(settings, "BRAND_TEXT", "")
+        'brand_text': getattr(settings, "BRAND_TEXT", "")
     }
 
 
 def site_title(request):
     return {
-        'brand_text' : getattr(settings, "SITE_TITLE", "")
+        'brand_text': getattr(settings, "SITE_TITLE", "")
     }
