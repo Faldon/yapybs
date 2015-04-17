5e3ecb1ad8fbeaba596f4fb6a3ba4195605341b9
Thomas Pulzer <t.pulzer@thesecretgamer.de>
2015-04-17 07:52:43 +0200

    Added comments and docstrings for better understanding. Slightly imoroved templates and fixed a bug with year_archive link to previous year.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -3,18 +3,33 @@ from django.conf import settings
 
 
 def current_date(request):
+    """
+    Returns the current date for using in templates
+    :param request: HTTP Request
+    :return:
+    """
     return {
         'current_date': datetime.date.today()
     }
 
 
 def brand_text(request):
+    """
+    Returns the brand_text from settings for using in templates
+    :param request:
+    :return:
+    """
     return {
         'brand_text': getattr(settings, "BRAND_TEXT", "")
     }
 
 
 def site_title(request):
+    """
+    Returns the site_title from settings for using in templates
+    :param request:
+    :return:
+    """
     return {
         'site_title': getattr(settings, "SITE_TITLE", "")
     }
