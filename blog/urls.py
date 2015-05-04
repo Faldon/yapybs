2560af28f7d49120632c3e39d1c301382f59b3d3
Thomas Pulzer <t.pulzer@thesecretgamer.de>
2015-05-04 20:04:32 +0200

    Re-ordered URL configuration for solving a bug in page count.

--- blog/urls.py
+++ blog/urls.py
@@ -3,19 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'rss$', views.rss, name='rss'),
-    url(r'search$', views.search, name='search'),
-    url(r'post/(?P<post_id>\d+)$', views.detail, name='detail'),
+    url(r'rss/$', views.rss, name='rss'),
+    url(r'search/$', views.search, name='search'),
     url(r'post/(?P<post_id>\d+)/$', views.detail, name='detail'),
-    url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$', views.day_archive, name='day_archive'),
     url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', views.day_archive, name='day_archive'),
-    url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})/page/(?P<page>\d)$', views.month_archive, name='month_archive'),
-    url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})$', views.month_archive, name='month_archive'),
+    url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})/page/(?P<page>\d)/$', views.month_archive, name='month_archive'),
     url(r'archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$', views.month_archive, name='month_archive'),
-    url(r'archive/(?P<year>\d{4})/page/(?P<page>\d)$', views.year_archive, name='year_archive'),
+    url(r'archive/(?P<year>\d{4})/page/(?P<page>\d)/$', views.year_archive, name='year_archive'),
     url(r'archive/(?P<year>\d{4})/$', views.year_archive, name='year_archive'),
-    url(r'archive/(?P<year>\d{4})$', views.year_archive, name='year_archive'),
+    url(r'page/(?P<page>\d+)/$', views.index, name='index'),
     url(r'$', views.index, name='index'),
-    url(r'page/(?P<page>\d)$', views.index, name='index'),
-    url(r'page/(?P<page>\d)/$', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
+import datetime
+
 from django.shortcuts import render, redirect
 from django.db.models import Q
+
 from .models import BlogPost
 from .forms import DatepickerForm
-import datetime
 
 
 def index(request, page='1'):
     """ Index View
 
     Selects the nth 5 posts where n is the given page number and renders the index page.
+
     :type request: django.http.HttpRequest
     :type page: string
     :param request: The http request sent from the client
@@ -16,7 +19,7 @@ def index(request, page='1'):
     :return: An html http response
     :rtype: django.http.HttpResponse
     """
-    selected_posts = BlogPost.objects.order_by('-published')[5*(int(page)-1):5*int(page)]
+    selected_posts = BlogPost.objects.order_by('-published')[(int(page) - 1) * 5:5 * int(page)]
 
     # Count total number of posts
     post_count = BlogPost.objects.count()
@@ -24,7 +27,7 @@ def index(request, page='1'):
     return render(request, 'blog/index.html', dict(
         selected_posts=selected_posts,
         current_page=int(page),
-        page_count=range(0, int(round(post_count/5)+1)))
+        page_count=range(0, int(round(post_count / 5) + 1)))
     )
 
 
@@ -32,6 +35,7 @@ def year_archive(request, year, page='1'):
     """ Year archive view
 
     Selects the nth 5 posts published in given year where n is the given page number and renders the year archive page.
+
     :type request: django.http.HttpRequest
     :type year: string
     :type page: string
@@ -44,20 +48,20 @@ def year_archive(request, year, page='1'):
     # Get posts published in the given year
     selected_posts = BlogPost.objects.filter(
         published__gte=datetime.date(int(year), 1, 1)).exclude(
-            published__gte=datetime.date(int(year)+1, 1, 1)
-        ).order_by('-published')[5*(int(page)-1):5*int(page)]
+        published__gte=datetime.date(int(year) + 1, 1, 1)
+    ).order_by('-published')[5 * (int(page) - 1):5 * int(page)]
 
     # Count number of posts in the given year
     post_count = BlogPost.objects.filter(
         published__gte=datetime.date(int(year), 1, 1)).exclude(
-            published__gte=datetime.date(int(year)+1, 1, 1)
-        ).count()
+        published__gte=datetime.date(int(year) + 1, 1, 1)
+    ).count()
 
     return render(request, 'blog/year_archive.html', dict(
         selected_posts=selected_posts,
         current_page=int(page),
         selected=datetime.date(int(year), 1, 1),
-        page_count=range(0, int(round(post_count/5)+1)))
+        page_count=range(0, int(round(post_count / 5) + 1)))
     )
 
 
@@ -66,6 +70,7 @@ def month_archive(request, year, month, page='1'):
 
     Selects the nth 5 posts published in given month and given year where n is the given page number
     and renders the month archive page.
+
     :type request: django.http.HttpRequest
     :type year: string
     :type month: string
@@ -92,16 +97,16 @@ def month_archive(request, year, month, page='1'):
     # Get posts published in the current month
     selected_posts = BlogPost.objects.filter(
         published__gte=current_month
-        ).exclude(
-            published__gte=next_month
-        ).order_by('-published')[5*(int(page)-1):5*int(page)]
+    ).exclude(
+        published__gte=next_month
+    ).order_by('-published')[5 * (int(page) - 1):5 * int(page)]
 
     # Count number of posts in the current month
     post_count = BlogPost.objects.filter(
         published__gte=current_month
-        ).exclude(
-            published__gte=next_month
-        ).count()
+    ).exclude(
+        published__gte=next_month
+    ).count()
 
     return render(request, 'blog/month_archive.html', dict(
         selected_posts=selected_posts,
@@ -109,7 +114,7 @@ def month_archive(request, year, month, page='1'):
         selected=current_month,
         previous_month=previous_month,
         next_month=next_month,
-        page_count=range(0, int(round(post_count/5)+1)))
+        page_count=range(0, int(round(post_count / 5) + 1)))
     )
 
 
@@ -118,6 +123,7 @@ def day_archive(request, year, month, day):
 
     Selects all posts published at date build by given year,month and day and renders the day archive page or
     processes the form data on POST and redirects to itself.
+
     :type request: django.http.HttpRequest
     :type year: string
     :type month: string
@@ -138,8 +144,8 @@ def day_archive(request, year, month, day):
         form = DatepickerForm()
         selected_posts = BlogPost.objects.filter(
             published__gte=datetime.date(int(year), int(month), int(day))).exclude(
-                published__gte=datetime.date(int(year), int(month), int(day)+1)
-            ).order_by('-published')
+            published__gte=datetime.date(int(year), int(month), int(day) + 1)
+        ).order_by('-published')
 
         return render(request, 'blog/day_archive.html', dict(
             selected_posts=selected_posts,
@@ -152,6 +158,7 @@ def detail(request, post_id):
     """ Post details view
 
     Selects the post with the given id and renders the post page.
+
     :type request: django.http.HttpRequest
     :type post_id: string
     :param request: The http request sent from the client
@@ -168,6 +175,7 @@ def rss(request):
     """ RSS feed view
 
     Returns an rss feed of the latest fifteen posts.
+
     :type request: django.http.HttpRequest
     :param request: The http request sent from the client
     :return: An xml http response
@@ -183,6 +191,7 @@ def search(request):
 
     Finds all blog posts which have a tag assigned equal to the keyword(s) submitted or which topics contains parts
     of the keyword(s) submitted
+
     :type request: django.http.HttpRequest
     :param request: The http request sent from the client
     :return: An html http response
